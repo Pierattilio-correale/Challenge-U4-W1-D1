dc6597789defadf6531cc7e86d9e3f3ba1892977
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-05-05 16:43:48 +0200

    adds modified

--- src/Esercizio2.java
+++ src/Esercizio2.java
@@ -1,24 +1,30 @@
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class Esercizio2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String parola = metodoMain(scanner);
-        System.out.println(parola);
 
+        System.out.println("Stringa normale: " + parola);
+
+        // Inversione dell'ordine delle parole
+        String[] parole = parola.split(" ");
+        String parolaInvertita = parole[2] + " " + parole[1] + " " + parole[0];
+        System.out.println("Stringa con ordine invertito: " + parolaInvertita);
     }
-    public static String metodoMain (Scanner scanner){
-        String a ;
-        String b ;
-        String c;
 
-        System.out.println("inserisci la prima parola");
-        a = scanner.nextLine(); ;
-        System.out.println("inserisci la seconda parola");
+    public static String metodoMain(Scanner scanner) {
+        String a, b, c;
+
+        System.out.println("Inserisci la prima parola:");
+        a = scanner.nextLine();
+        System.out.println("Inserisci la seconda parola:");
         b = scanner.nextLine();
-        System.out.println("inserisci la terza parola");
-        c= scanner.nextLine();
-        return a + " " +b +" "+ c;
+        System.out.println("Inserisci la terza parola:");
+        c = scanner.nextLine();
 
+        return a + " " + b + " " + c;
     }
 }
